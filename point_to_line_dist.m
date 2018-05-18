3a2583bce42611280dfb62c4977083cda667745b
Stephanie Lackner <slackner@princeton.edu>
2018-05-17 23:14:10 -0400

    Add point_to_line_dist function

---
+++ point_to_line_dist.m
@@ -0,0 +1,11 @@
+function [ d] = point_to_line_dist(pt, v1, v2)
+%Shortest distance from point pt to line going through points v1 and v2 in a 3D space
+%Suggestion from: https://de.mathworks.com/matlabcentral/answers/95608-is-there-a-function-in-matlab-that-calculates-the-shortest-distance-from-a-point-to-a-line
+
+      a = v2 - v1;
+
+      b = pt - v1;
+
+      d = norm(cross(a,b)) / norm(a);
+end
+
